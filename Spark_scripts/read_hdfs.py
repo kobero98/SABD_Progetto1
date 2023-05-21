8e2dc7a15d0084f6c5bc98e442d58ee3c1704c6b
kobero98 <matteo.fede@live.it>
2023-05-21 15:07:05 +0000

    inizio prima query

--- Spark_scripts/read_hdfs.py
+++ Spark_scripts/read_hdfs.py
@@ -2,7 +2,7 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import col
 import sys,logging
 from datetime import datetime
-
+import pandas as pd
 # Logging configuration
 formatter = logging.Formatter('[%(asctime)s] %(levelname)s @ line %(lineno)d: %(message)s')
 handler = logging.StreamHandler(sys.stdout)
@@ -24,14 +24,19 @@ def main():
     spark.sparkContext.setLogLevel("ERROR")
     logger.info("Starting spark application")
     print("ciao\n")
+
     #do something here
     logger.info("Reading CSV File")
     df_category = spark.read.option("header","true").csv("hdfs://master:54310/cartellaNIFI/out500_combined+header.csv")
-    print(df_category.dtypes)
-    df_category2 = df_category.filter(df_category.SecType == "E")
-    logger.info("Previewing CSV File Data")
-    df_category2.show(truncate=False)
-    logger.info("Ending spark application")
+    df_category.createOrReplaceTempView("view1")
+    result = spark.sql("SELECT ID,count(*)  FROM view1 where SecType='E' and ID LIKE '%.FR' GROUP BY ID")
+    result.show()
+    print(result)
+   # df_category2 = df_category.filter(df_category.SecType == "E")
+   # df_category3 = df_category2.loc[df_category2["ID"].str.endswith(".FR")]
+   # logger.info("Previewing CSV File Data")
+   # df_category3.show(truncate=False)
+   # logger.info("Ending spark application")
     # end spark code
     spark.stop()
     return None

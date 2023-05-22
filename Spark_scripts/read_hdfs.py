4b691d7875363b345a5e1f2275aeb16a35cd867a
kobero98 <matteo.fede@live.it>
2023-05-22 19:21:27 +0000

    prima query

--- Spark_scripts/read_hdfs.py
+++ Spark_scripts/read_hdfs.py
@@ -11,11 +11,11 @@ handler.setFormatter(formatter)
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 logger.addHandler(handler)
-
 # current time variable to be used for logging purpose
 dt_string = datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
 # change it to your app name
 AppName = "MyPySparkApp"
+
 def query1filtrer(f):
     return f[1] == "FR" and f[2] == "E"
 def query1map(f):
@@ -23,24 +23,23 @@ def query1map(f):
     app = x[0].split(".")
     hour = x[3].split(sep=":")
     if (len(app)>1):
-        y=[app[0],app[1],x[1],float(x[2]),x[4],x[4]+hour[0]+app[0],x[3]]
+        y=[app[0],app[1],x[1],float(x[2]),x[4],x[4]+"/"+hour[0]+"/"+app[0],x[3]]
     else:
-        y=["Nome","paese",x[1],x[2],x[4],"data-ora-ID",x[3]]
+        y=["Nome","paese",x[1],x[2],x[4],"data/ora/ID",x[3]]
     return y
 def query1Min(f,x):
-    if f[3]<x[3]:
-        return f[3]
+    if f<x:
+        return f
     else:
-        return x[3]
+        return x
 def query1Max(f,x):
-    if f[3]>x[3]:
-        return f[3]
+    if f>x:
+        return f
     else:
-        return x[3]
-def minMap(f):
-    for i in f[1]:
-        m=i[3]
-    return f[0],m;
+        return x
+def funcQ1(f):
+    x=f[0].split(sep="/")
+    return [x[0],x[1],x[2]+".FR",f[1],f[3],f[2],f[4]]
 def main():
     # start spark code
     spark = SparkSession.builder.appName(AppName+"_"+str(dt_string)).getOrCreate()
@@ -51,17 +50,15 @@ def main():
     logger.info("Reading CSV File")
     #df_category = spark.read.option("header","true").csv("hdfs://master:54310/cartellaNIFI/out500_combined+header.csv")
     #print(df_category.dtypes)
-    rdd1 = spark.sparkContext.textFile("hdfs://master:54310/cartellaNIFI/out500_combined+header.csv").map(query1map).filter(query1filtrer)
-    rdd2 = rdd1.groupBy(lambda f: f[5]).cache()
-    minimoVal = rdd2.reduceByKey(query1Min).map(minMap)
-    massimoVal = rdd2.reduceByKey(query1Max).map(minMap)
-    mediaVal = rdd2.reduceByKey(lambda x,y: y+x[3])
-    for i in mediaVal.collect():
-        for j in i[1]:
-            print(i,j)
-    #max1 = rdd1.max(lambda f : f[3])
-    # min1 = rdd1.min(lambda f : f[3])
-    #print(type(max1),type(min1))
+    rdd1 = spark.sparkContext.textFile("hdfs://master:54310/cartellaNIFI/out500_combined+header.csv").map(query1map).filter(query1filtrer).map(lambda f: [f[5],f[3]]).cache()
+    minVal = rdd1.reduceByKey(query1Min)
+    maxVal = rdd1.reduceByKey(query1Max)
+    sommVal = rdd1.reduceByKey(lambda x,y:x+y)
+    countVal = rdd1.map(lambda x: [x[0],1]).reduceByKey(lambda x,y:x+y)
+    pars = minVal.fullOuterJoin(maxVal).fullOuterJoin(sommVal).fullOuterJoin(countVal).map(lambda f: [f[0],f[1][0][0][0],f[1][0][0][1],f[1][0][1]/f[1][1],f[1][1]])
+    resultQ1 = pars.map(funcQ1)
+    resultQ1.map(lambda f: f[0]+","+f[1]+","+f[2]+","+str(f[3])+","+str(f[4])+","+str(f[5])).saveAsTextFile("hdfs://master:54310/cartellaResult/Query1Result.csv") 
+    
     # x = rdd1.collect()
     #for i in x: print(i)
     #df_category.createOrReplaceTempView("view1")

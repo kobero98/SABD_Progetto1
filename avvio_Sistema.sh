c2bc45279972554826a7cf9f69b250673238a313
kobero98 <matteo.fede@live.it>
2023-06-10 15:07:52 +0000

    inseriemento automatizzazione di grafana e dashboard query1 e 2

--- avvio_Sistema.sh
+++ avvio_Sistema.sh
@@ -2,7 +2,9 @@
 cd ./DockerFile/Spark/
 docker build -t cluster-apache-spark:3.0.2 .
 cd ../..
+cd ./DockerFile/Grafana/
+docker build -t grafkob .
+cd ../..
 docker-compose build
 docker-compose up -d
 docker exec master sh /home/data/avvio_hdfs.sh
-docker logs nifi | grep Generated > profileNifi.txt

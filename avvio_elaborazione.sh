fe7611d0475e1b057ddda8ad91ac0705d9681f39
kobero98 <matteo.fede@live.it>
2023-06-12 15:23:35 +0000

    sistemazione repository

--- avvio_elaborazione.sh
+++ avvio_elaborazione.sh
@@ -1,10 +1,9 @@
-#docker exec -it spark_master /bin/bash ../spark-apps/start_app.sh
+docker exec -it spark_master sh ../spark-apps/start_app.sh
 sudo rm -r -d ./DockerFile/HDFS/cartellaResult
 docker exec -it master hdfs dfs -get /cartellaResult /home/data 
-sudo rm -d -r ./Result
-mkdir Result
-sudo mv ./DockerFile/HDFS/cartellaResult/Query1Result/*.csv ./Result/Query1.csv
-sudo mv ./DockerFile/HDFS/cartellaResult/Query2ResultBot/*.csv ./Result/Query2Bot.csv
-sudo mv ./DockerFile/HDFS/cartellaResult/Query2ResultTop/*.csv ./Result/Query2Top.csv
-sudo mv ./DockerFile/HDFS/cartellaResult/Query3Result/*.csv  ./Result/Query3.csv
-python3 -m http.server 
+sudo rm ./Results/*
+mkdir Results
+sudo mv ./DockerFile/HDFS/cartellaResult/Query1Result/*.csv ./Results/Query1.csv
+sudo mv ./DockerFile/HDFS/cartellaResult/Query2ResultBot/*.csv ./Results/Query2Bot.csv
+sudo mv ./DockerFile/HDFS/cartellaResult/Query2ResultTop/*.csv ./Results/Query2Top.csv
+sudo mv ./DockerFile/HDFS/cartellaResult/Query3Result/*.csv  ./Results/Query3.csv
